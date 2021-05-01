290655075d5284cbd2e10118f272b93e5bbf5cc9
Angela Man <angela.k.man@gmail.com>
2021-04-30 23:42:29 -0500

    use forEach

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArr = process.argv.slice(2, process.argv.length);
 //console.log(profileDataArgs);
 
-const printProfileData = (profileDataArgs) => {
-    for (let i=0; i < profileDataArgs.length; i++) {
-        console.log(profileDataArgs[i]);
+const printProfileData = profileDataArr => {
+    for (let i=0; i < profileDataArr.length; i+=1) {
+        console.log(profileDataArr[i]);
     }
+
+    console.log("============");
+
+    profileDataArr.forEach((profileItem) => {
+        console.log(profileItem)
+    });
 };
 
-printProfileData(profileDataArgs);
+printProfileData(profileDataArr);
