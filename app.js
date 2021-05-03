2baf422e49e04b5a3a5cc0166efb137473447932
Angela Man <angela.k.man@gmail.com>
2021-05-02 22:57:50 -0500

    create project HTML section

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
-// const fs = require("fs");
-// const generatePage = require("./src/page-template.js");
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -140,15 +140,14 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile("./index.html", pageHTML, err => {
+            if (err) throw new Error (err);
+
+            console.log("Portfolio complete! Check out index.html to see the output!");
+        });
     });
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile("./index.html", pageHTML, err => {
-//     if (err) throw new Error (err);
-
-//     console.log("Portfolio complete! Check out index.html to see the output!");
-// });
 
 //console.log(profileDataArgs);
 
